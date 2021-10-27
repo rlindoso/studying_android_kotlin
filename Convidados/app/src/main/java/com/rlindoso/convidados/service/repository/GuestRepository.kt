a1a46915fdab2e5f4c3c9de0ba4faa1b56d3f02b
Rafael Lindoso <rafael.lindoso@gmail.com>
2021-10-26 22:26:46 -0300

    Update guest

--- Convidados/app/src/main/java/com/rlindoso/convidados/service/repository/GuestRepository.kt
+++ Convidados/app/src/main/java/com/rlindoso/convidados/service/repository/GuestRepository.kt
@@ -54,8 +54,23 @@ class GuestRepository private constructor(context: Context) {
         }
     }
 
-    fun update(guest: GuestModel) {
+    fun update(guest: GuestModel): Boolean {
+        return try {
+            val db = mGuestDataBaseHelper.writableDatabase
 
+            val contentValues = ContentValues()
+            contentValues.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            contentValues.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, guest.presence)
+
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " =?"
+            val args = arrayOf(guest.id.toString())
+
+            db.update(DataBaseConstants.GUEST.TABLE_NAME, contentValues, selection, args)
+
+            true
+        } catch (e: Exception) {
+            false
+        }
     }
 
     fun delete(guest: GuestModel) {
